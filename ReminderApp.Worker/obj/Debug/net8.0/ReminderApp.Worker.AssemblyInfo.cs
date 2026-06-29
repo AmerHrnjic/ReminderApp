5b4e850958de4b071fd04439d97d727a98429004
Amer Hrnjic <amerhrnjo16@gmail.com>
2026-06-29 12:06:56 +0200

    Added .env.exmple files

--- ReminderApp.Worker/obj/Debug/net8.0/ReminderApp.Worker.AssemblyInfo.cs
+++ ReminderApp.Worker/obj/Debug/net8.0/ReminderApp.Worker.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReminderApp.Worker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dae92f0d806e2910fb44ef9a956ab0fa65321c86")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReminderApp.Worker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReminderApp.Worker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
